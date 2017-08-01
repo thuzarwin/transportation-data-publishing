f38dcdc5626be72800a6cb22fdcee5d0f53f1f06
John Clary <john.clary@austintexas.gov>
2017-08-01 15:33:32 -0500

    WIP: original syntax

--- gridsmart/gridsmart_api.py
+++ gridsmart/gridsmart_api.py
@@ -17,15 +17,19 @@ class Gridsmart:
     ----------
     ip : string (required)
         IP address of gridsmart camerasite
+
     port : int or string (default: 8902)
         Port to reach site endpoint
+
     timeout : numeric (default: 10)
         number of seconds before http request timeout
+
     get_zones : bool (default: False)
         extract zone information from site config and append as dict to self.zones
+
     fieldmap_zones : dict (default: None)
-        when provided, keys in zone data will be replaced according to field map
-        see method for additional details
+        a dict of format { input_key : output_key }
+        when provided, keys in zone data will be replaced according to field mapwhen provided, keys in zone data will be replaced according to field map
     '''
     def __init__(self, ip, port=8902, timeout=10, get_zones=False, fieldmap_zones=None):
         self.ip = ip
@@ -34,13 +38,13 @@ class Gridsmart:
         self.timeout = float(timeout)
 
         self.id = None
-        self.getId(self.endpoint, self.timeout)
+        self.getId()
 
         self.name = None
-        self.getName(self.endpoint, self.timeout)
+        self.getName()
 
         self.config = None
-        self.getConfig(self.endpoint, self.timeout)
+        self.getConfig()
 
         self.zones = None
         
@@ -51,12 +55,12 @@ class Gridsmart:
             
             if fieldmap_zones:
                 for zone in self.zones:
-                    self.zones[zone] = self.mapFields(self.zones[zone], self.fieldmap_zones)
+                    self.zones[zone] = self.mapFields(self.zones[zone])
 
         self.days_available = None
-        self.getDaysAvailable(self.endpoint, self.timeout)
+        self.getDaysAvailable()
 
-    def getId(self, endpoint, timeout):
+    def getId(self):
         '''
         Copied from gtipy
         https://bitbucket.org/GRIDSMART/public-gtipy
@@ -65,10 +69,7 @@ class Gridsmart:
 
         Parameters
         ----------
-        endpoint : string
-            the API endpoint (URL) at which to retrieve the site id
-        timeout : numeric (default: 10)
-            number of seconds before http request timeout
+        None
 
         Returns
         -------
@@ -86,7 +87,7 @@ class Gridsmart:
         self.id = dashify_site_or_zone_id(site_id)
         return self.id
 
-    def getName(self, endpoint, timeout):
+    def getName(self):
         '''
         Copied from gtipy
         https://bitbucket.org/GRIDSMART/public-gtipy
@@ -95,25 +96,22 @@ class Gridsmart:
 
         Parameters
         ----------
-        endpoint : string
-            the API endpoint (URL) at which to retrieve the site id
-        timeout : numeric (default: 10)
-            number of seconds before http request timeout
+        None
 
         Returns
         -------
         value : string
             The site name
         '''
-        ep = endpoint + 'sitename.json'
-        res = requests.get(ep, timeout=timeout)
+        ep = self.endpoint + 'sitename.json'
+        res = requests.get(ep, timeout=self.timeout)
         if res.status_code != 200:
             raise Exception('failed to get site name')
         
         self.name = res.json().encode('ascii')
         return self.name
 
-    def getConfig(self, endpoint, timeout):
+    def getConfig(self):
         '''
         Copied from gtipy
         https://bitbucket.org/GRIDSMART/public-gtipy
@@ -124,6 +122,7 @@ class Gridsmart:
         ----------
         endpoint : string
             the API endpoint (URL) at which to retrieve the site id
+
         timeout : numeric (default: 10)
             number of seconds before http request timeout
 
@@ -132,14 +131,14 @@ class Gridsmart:
         config : dict
             Dictionary created from site config JSON
         '''
-        ep = endpoint + 'site.json'
-        res = requests.get(ep, timeout=timeout)
+        ep = self.endpoint + 'site.json'
+        res = requests.get(ep, timeout=self.timeout)
         if res.status_code != 200:
             raise Exception('failed to get site config')
         self.config = res.json()
         return self.config      
 
-    def getZones(self, config):
+    def getZones(self):
         '''
         Copied from gtipy
         https://bitbucket.org/GRIDSMART/public-gtipy
@@ -148,8 +147,7 @@ class Gridsmart:
 
         Parameters
         ----------
-        config : dict (required)
-            a GRIDSMART site configuration dictionary (see getConfig method)
+        None
 
         Returns
         -------
@@ -158,7 +156,7 @@ class Gridsmart:
         '''
         vehicle_zones = {}
         
-        site_id = dashify_site_or_zone_id(config['Id'])
+        site_id = dashify_site_or_zone_id(self.config['Id'])
         config['Id'] = site_id
 
         for cam in config['CameraDevices']:
@@ -176,7 +174,7 @@ class Gridsmart:
         self.zones = vehicle_zones
         return self.zones
 
-    def mapFields(self, obj, fieldmap):
+    def mapFields(self, obj):
         '''
         Lookup and replace keys in a dictioanry from an input fieldmap.
 
@@ -187,8 +185,6 @@ class Gridsmart:
         ----------
         obj : dict (required)
             a dict whose (top-level) keys are to be replaced
-        fieldmap : dict (required)
-            a dict of format { input_key : output_key } which will be used to replace the input dict's fields
 
         Returns
         -------
@@ -203,8 +199,8 @@ class Gridsmart:
                 continue
 
             # lookup and replace key
-            if key in fieldmap:
-                obj[fieldmap[key]] = obj.pop(key)
+            if key in self.fieldmap:
+                obj[self.fieldmap[key]] = obj.pop(key)
             else:
                 #  drop fields not in fieldmap
                 obj.pop(key)
@@ -212,15 +208,17 @@ class Gridsmart:
         return obj
 
     def getDaysAvailable(self, endpoint, timeout, first_day=None, last_day=None, sort_descending=True):
-        """
+        '''
         Get the list of count days available in given range
 
         Parameters
         ----------
         first_day : string (defaults to 2010-01-01)
             First day in form YYYY-MM-DD
+
         last_day : string (defaults to yesterday)
             Last day in form YYYY-MM-DD
+
         sort_descending : boolean (optional, default = True)
             List is sorted descending by default.
 
@@ -228,8 +226,7 @@ class Gridsmart:
         -------
         dates_available : [string]
             List of strings of form 'YYYY-MM-DD'
-        """
-
+        '''
         r_days_available = requests.get(endpoint + 'counts.json', timeout=timeout)
 
         if r_days_available.status_code != 200:
@@ -267,6 +264,39 @@ class Gridsmart:
 
         self.days_available = days_available
         return self.days_available
+
+    def getCounts(self, day_str, local_file = None):
+        '''
+        Download the requested day of data to the location specified.
+
+        Parameters
+        ----------
+        day_str : string
+            Date of the data to download in form 'YYYY-MM-DD'
+
+        local_file : string (optional, default = None)
+            Local file of the downloaded data. If none is provided,
+            file will be written to './<site_id>/zips/<YYYY-MM-DD>.zip'
+
+        Returns
+        -------
+        success : bool
+            Whether or not the download succeeded.
+        '''
+        ep = self.endpoint + 'counts/bydate/' + day_str
+
+        if local_file is None:
+            site_path = os.path.join( os.path.abspath(os.curdir), self.id )
+            create_dir(site_path)
+            zips_path = os.path.join( site_path, 'zips' )
+            create_dir(zips_path)
+            local_file = os.path.join(zips_path, day_str + '.zip')
+
+        r = requests_get_by_chunks(ep, local_file)
+
+        success = (r.status_code == 200)
+        return success
+
 
 #  helper methods
 def dashify_site_or_zone_id(id_):
